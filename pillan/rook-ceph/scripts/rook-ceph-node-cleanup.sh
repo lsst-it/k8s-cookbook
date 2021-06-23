b6c24129d7c23538f5964bbc98b4cb01a5261f1f
Joshua Hoblitt <josh@hoblitt.com>
2021-06-23 09:58:04 -0700

    (pillan) update CephCluster to new hardware

--- pillan/rook-ceph/scripts/rook-ceph-node-cleanup.sh
+++ pillan/rook-ceph/scripts/rook-ceph-node-cleanup.sh
@@ -6,11 +6,12 @@ SSH_USER=hreinking_b
 
 HOST="pillan01.ls.lsst.org"
 SSH_CMD="ssh ${HOST} -l ${SSH_USER}"
+DEV=/dev/nvme1n1
 
 ${SSH_CMD} sudo rm -rf /var/lib/rook
 ${SSH_CMD} sudo sh -c 'ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %'
 ${SSH_CMD} sudo rm -rf /dev/ceph-*
-${SSH_CMD} sudo sgdisk --zap-all /dev/sdb
-${SSH_CMD} sudo dd if=/dev/zero of=/dev/sdb bs=1M count=100 oflag=direct,dsync
-${SSH_CMD} sudo blockdev --rereadpt /dev/sdb
+${SSH_CMD} sudo sgdisk --zap-all "$DEV"
+${SSH_CMD} sudo dd if=/dev/zero "of=${DEV}" bs=1M count=100 oflag=direct,dsync
+${SSH_CMD} sudo blockdev --rereadpt "$DEV"
 ${SSH_CMD} sudo reboot
