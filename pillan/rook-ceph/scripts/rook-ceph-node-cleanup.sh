0593b6987dc55b534dfea04ba359db312e47faf2
Joshua Hoblitt <josh@hoblitt.com>
2021-07-12 09:42:54 -0700

    (pillan) reboot after ceph blockdev cleanup

--- pillan/rook-ceph/scripts/rook-ceph-node-cleanup.sh
+++ pillan/rook-ceph/scripts/rook-ceph-node-cleanup.sh
@@ -5,7 +5,12 @@ set -x
 DEV=/dev/nvme1n1
 SSH_CMD='clush -g pillan'
 
-${SSH_CMD} sudo /bin/rm -rf /var/lib/rook
+${SSH_CMD} sudo rm -rf /var/lib/rook
 ${SSH_CMD} 'ls /dev/mapper/ceph-* | xargs -I% -- echo /sbin/dmsetup remove %'
-${SSH_CMD} sudo /bin/rm -rf /dev/ceph-*
-${SSH_CMD} sudo /sbin/sgdisk --zap-all "$DEV"
+${SSH_CMD} sudo rm -rf /dev/ceph-*
+${SSH_CMD} sudo sgdisk --zap-all "$DEV"
+${SSH_CMD} sudo dd if=/dev/zero "of=${DEV}" bs=1M count=100 oflag=direct,dsync
+${SSH_CMD} sudo blockdev --rereadpt "$DEV"
+${SSH_CMD} lsblk
+# sometimes dm devices aren't cleaned up for unknown reason(s)
+${SSH_CMD} sudo reboot
