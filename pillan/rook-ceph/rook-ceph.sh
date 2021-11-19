7cce9901ab9d35c793e6792e068ee5f136f1b970
Joshua Hoblitt <josh@hoblitt.com>
2021-11-19 10:15:43 -0700

    (pillan) add lfa/s3 to rook-ceph.sh

--- pillan/rook-ceph/rook-ceph.sh
+++ pillan/rook-ceph/rook-ceph.sh
@@ -45,6 +45,10 @@ kubectl apply -f cephfs-lsstdata.yaml
 kubectl apply -f cephfs-project.yaml
 kubectl apply -f cephfs-scratch.yaml
 
+# lfa/s3
+kubectl apply -f s3/object_store.yaml
+kubectl apply -f s3/ingress.yaml
+
 # no spaces after `,`s is allowed
 kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') -- \
 ceph dashboard set-ganesha-clusters-rados-pool-namespace \
