ad5a3da96e5c902e93bf686fd1bc613ec10b4d89
Heinrich Reinking <hreinkinge@gmail.com>
2022-04-05 16:56:18 -0400

    enable nfs storages

--- gaw/rook-ceph/rook-ceph.sh
+++ gaw/rook-ceph/rook-ceph.sh
@@ -39,8 +39,19 @@ echo "===================="
 kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo
 echo "===================="
 
-# it/s3
-kubectl apply -f s3/object_store.yaml
-kubectl apply -f s3/ingress.yaml
+# cephfs w/ nfs
+kubectl apply -f nfs/cephfs-backup.yaml
+kubectl apply -f nfs/cephfs-data.yaml
+kubectl apply -f nfs/cephfs-home.yaml
 
+# no spaces after `,`s is allowed
+kubectl -n rook-ceph exec -it "$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')" -- \
+ceph dashboard set-ganesha-clusters-rados-pool-namespace \
+backup:nfs-ganesha/backup,\
+data:nfs-ganesha/data,\
+home:nfs-ganesha/home
+
+# XXX at least rook 1.7.[78] do not set the application type on the nfs-ganesha pool. This causes a ceph health warning.
+kubectl -n rook-ceph exec -it "$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')" -- \
+  ceph osd pool application enable nfs-ganesha nfs
 # vim: tabstop=2 shiftwidth=2 expandtab
