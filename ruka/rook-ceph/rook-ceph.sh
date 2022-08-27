f80546c938ef129daf17fa4c94e4a4e045d0ab9a
Joshua Hoblitt <josh@hoblitt.com>
2022-08-27 08:48:14 -0700

    (ruka) rook-ceph.sh: check for required commands

--- ruka/rook-ceph/rook-ceph.sh
+++ ruka/rook-ceph/rook-ceph.sh
@@ -4,10 +4,49 @@ set -ex
 
 VERSION='1.9.9'
 
+print_error() {
+  >&2 echo -e "$@"
+}
+
+fail() {
+  local code=${2:-1}
+  [[ -n $1 ]] && print_error "$1"
+  # shellcheck disable=SC2086
+  exit $code
+}
+
+has_cmd() {
+  local command=${1?command is required}
+  command -v "$command" > /dev/null 2>&1
+}
+
+require_cmds() {
+  local cmds=("${@?at least one command is required}")
+  local errors=()
+
+  # accumulate a list of all missing commands before failing to reduce end-user
+  # install/retry cycles
+  for c in "${cmds[@]}"; do
+    if ! has_cmd "$c"; then
+      errors+=("prog: ${c} is required")
+    fi
+  done
+
+  if [[ ${#errors[@]} -ne 0 ]]; then
+    for e in "${errors[@]}"; do
+      print_error "$e"
+    done
+
+    fail 'failed because of missing commands'
+  fi
+}
+
 cephtoolbox() {
   # shellcheck disable=SC2068
   kubectl -n rook-ceph exec -it "$(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}')" -- $@
 }
+
+require_cmds helm kubectl
 
 helm repo add rook-release https://charts.rook.io/release
 helm repo update
