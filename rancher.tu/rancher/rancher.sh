be80ce5fec4738a48e45c4f789efd004e08e714f
Joshua Hoblitt <josh@hoblitt.com>
2022-09-01 12:05:51 -0700

    (rancher.tu) rancher.sh: add --atomic flag & mv hostname to values.yaml

--- rancher.tu/rancher/rancher.sh
+++ rancher.tu/rancher/rancher.sh
@@ -6,10 +6,9 @@ helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm repo update
 
 helm upgrade --install \
+  --atomic \
   rancher rancher-stable/rancher \
   --create-namespace --namespace cattle-system \
-  --set hostname=rancher.tu.lsst.org \
-  --set ingress.tls.source=secret \
   --version v2.6.6 \
   -f ./values.yaml
 
