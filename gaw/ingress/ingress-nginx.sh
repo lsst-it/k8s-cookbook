96c00f89a3c6f5f91c338c80a9048950c0b35195
Carlos Barria <cbarria@lsst.org>
2022-09-21 18:25:41 -0300

    (gaw) change name and version to ingress-nginx.sh v4.2.1

--- gaw/ingress/ingress-nginx.sh
+++ gaw/ingress/ingress-nginx.sh
@@ -8,6 +8,7 @@ helm repo update
 helm upgrade --install \
   ingress-nginx ingress-nginx/ingress-nginx \
   --create-namespace --namespace ingress-nginx \
+  --version v4.2.1 \
   --set controller.kind=DaemonSet \
   --set defaultBackend.replicaCount=3 \
   --set rbac.create=true
