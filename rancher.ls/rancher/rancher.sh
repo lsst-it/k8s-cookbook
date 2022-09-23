c9fb15f48d733254e39656f351161177cffa92db
Carlos Barria <cbarria@lsst.org>
2022-09-23 16:53:13 -0300

    (rancher.ls) bump rancher helm version to 2.6.6 and add values.yml

---
+++ rancher.ls/rancher/rancher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm repo update
+
+helm upgrade --install \
+  --atomic \
+  rancher rancher-stable/rancher \
+  --create-namespace --namespace cattle-system \
+  --version v2.6.6 \
+  -f ./values.yaml
+
+kubectl -n cattle-system rollout status deploy/rancher
