6a82fc9f64694580ff5696ee2f3059e1c09938e2
Carlos Barria <cbarria@lsst.org>
2022-10-21 10:01:20 -0300

    (ruka) remove pod security on namespace

--- ruka/metallb/metallb.sh
+++ ruka/metallb/metallb.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 set -ex
-
-kubectl create namespace metallb-system
-kubectl label namespace metallb-system pod-security.kubernetes.io/enforce: privileged
-kubectl label namespace metallb-system pod-security.kubernetes.io/audit: privileged
-kubectl label namespace metallb-system pod-security.kubernetes.io/warn: privileged
-
 helm repo add metallb https://metallb.github.io/metallb
 helm upgrade --install --namespace metallb-system \
     --create-namespace \
