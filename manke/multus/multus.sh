a5a2662886baada635429d04766dbb2fc1774d1b
Joshua Hoblitt <josh@hoblitt.com>
2023-02-14 17:21:11 -0700

    (manke) add multus w/ lhn NAD

---
+++ manke/multus/multus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+( set -ex
+  git clone https://github.com/k8snetworkplumbingwg/multus-cni -b v3.7.1
+  cd multus-cni
+  kubectl apply -f images/multus-daemonset.yml
+)
+
+kubectl apply -f multus-nad-lhn.yaml
+
+# vim: tabstop=2 shiftwidth=2 expandtab
