8f8ee6bc35d0bdc078765ac5b17ae17dc7bc9d8e
Joshua Hoblitt <josh@hoblitt.com>
2023-02-27 17:16:53 -0700

    (yagan) update multus demo

--- yagan/multus/demo/demo.sh
+++ yagan/multus/demo/demo.sh
@@ -2,10 +2,18 @@
 
 set -ex
 
-kubectl apply -f multus-demo.yaml
-kubectl wait --for=condition=ready pod -l app=multus-demo
+NS=(
+  multus-demo-dds
+  multus-demo-lhn
+)
 
-kubectl logs multus-demo -c ddsnet4u
-kubectl exec -ti multus-demo -c iperf -- ip route
+kubectl apply -f multus-demo.yaml
+
+for n in "${NS[@]}"; do
+  echo "### NS: ${n} ###"
+  kubectl wait --for=condition=ready pod -n "$n" -l app=multus-demo
+  kubectl -n "$n" exec -ti multus-demo -c iperf -- ip route
+  echo
+done
 
 kubectl delete -f multus-demo.yaml
