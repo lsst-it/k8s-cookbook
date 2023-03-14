8d07c10c73f85026ee65bafdb0b7eae9244342b1
Carlos Barria <cbarria@lsst.org>
2023-03-14 13:11:50 -0300

    (gaw) bump metallb to 0.13.9

--- gaw/metallb/metallb.sh
+++ gaw/metallb/metallb.sh
@@ -5,5 +5,5 @@ helm repo add metallb https://metallb.github.io/metallb
 helm upgrade --install --namespace metallb-system \
      --create-namespace \
     metallb metallb/metallb \
-    --atomic --version 0.13.7
+    --atomic --version 0.13.9
 kubectl apply -f ippool.yaml
