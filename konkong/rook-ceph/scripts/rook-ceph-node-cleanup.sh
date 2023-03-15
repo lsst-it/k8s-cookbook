6979d3a2a93cbd00322e259ba5ca248bd903388b
Carlos Barria <cbarria@lsst.org>
2023-03-15 15:19:10 -0300

    (konkong) setup rook-ceph for s3

---
+++ konkong/rook-ceph/scripts/rook-ceph-node-cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+DEVS=(
+  nvme0n1
+  nvme1n1
+)
+
+HOSTS=(
+  konkong03
+  konkong02
+  konkong01
+)
+
+for H in "${HOSTS[@]}"; do
+  SSH_CMD="ssh ${H}"
+
+  ${SSH_CMD} sudo rm -rf /var/lib/rook
+  ${SSH_CMD} 'ls /dev/mapper/ceph-* | xargs -I% -- echo /sbin/dmsetup remove %'
+  ${SSH_CMD} sudo rm -rf /dev/ceph-*
+  for D in "${DEVS[@]}"; do
+    ${SSH_CMD} sudo sgdisk --zap-all "/dev/${D}"
+    ${SSH_CMD} sudo dd if=/dev/zero "of=/dev/${D}" bs=1M count=100 oflag=direct,dsync
+    ${SSH_CMD} sudo blockdev --rereadpt "/dev/${D}"
+  done
+  ${SSH_CMD} lsblk
+  # sometimes dm devices aren't cleaned up for unknown reason(s)
+  ${SSH_CMD} sudo reboot
+done
