be1e2935a8d0bc77d7c3f51140327a99738bd7d2
Carlos Barria <cbarria@lsst.org>
2023-06-19 20:12:11 -0400

    (rancher.dev) Add ingress-nginx

---
+++ rancher.dev/ingress/ingress-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm upgrade --install \
+  ingress-nginx ingress-nginx/ingress-nginx \
+  --create-namespace --namespace ingress-nginx \
+  --version v4.2.1 \
+  --set controller.kind=DaemonSet \
+  --set defaultBackend.replicaCount=3 \
+  --set rbac.create=true \
+  --set controller.config.proxy-body-size=10m \
+  --atomic
