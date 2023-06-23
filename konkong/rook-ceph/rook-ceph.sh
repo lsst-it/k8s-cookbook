6756b8add7861e234baae27c8f35321d5468199f
Joshua Hoblitt <josh@hoblitt.com>
2023-06-23 15:28:40 -0700

    (konkong) update rook-ceph.sh for reliability + device monitoring

--- konkong/rook-ceph/rook-ceph.sh
+++ konkong/rook-ceph/rook-ceph.sh
@@ -49,17 +49,29 @@ ceph() {
   cephtoolbox ceph "$@"
 }
 
-waitfor() {
+# run command until it exits 0
+waitforcmd() {
+  xtrace=$(set +o|grep xtrace); set +x
+  local wait=${1?sleep interval}; shift
+
+  echo "Waiting for $*"
+
+  until "$@" > /dev/null 2>&1; do
+    echo "Waiting for $*"
+    sleep "$wait";
+  done
+
+  eval "$xtrace"
+}
+
+waitforkube() {
   xtrace=$(set +o|grep xtrace); set +x
   local ns=${1?namespace is required}; shift
   local type=${1?type is required}; shift
 
-  echo "Waiting for $type $*"
   # wait for resource to exist. See: https://github.com/kubernetes/kubernetes/issues/83242
-  until kubectl -n "$ns" get "$type" "$@" -o=jsonpath='{.items[0].metadata.name}' >/dev/null 2>&1; do
-    echo "Waiting for $type $*"
-    sleep 1
-  done
+  waitforcmd 2 kubectl -n "$ns" get "$type" "$@" -o=jsonpath='{.items[0].metadata.name}'
+
   eval "$xtrace"
 }
 
@@ -68,10 +80,22 @@ waitforpod() {
   local ns=${1?namespace is required}; shift
 
   # wait for pod to exist
-  waitfor "$ns" pod "$@"
+  waitforkube "$ns" pod "$@"
 
   # wait for pod to be ready
   kubectl -n rook-ceph wait --for=condition=ready --timeout=180s pod "$@"
+  eval "$xtrace"
+}
+
+# wait for ceph nfs related resources to be ready
+waitfornfs() {
+  xtrace=$(set +o|grep xtrace); set +x
+  local fs=${1?fs name}; shift
+
+  waitforpod rook-ceph -l app=rook-ceph-nfs,ceph_nfs="$fs"
+  waitforpod rook-ceph -l app=rook-ceph-mds,rook_file_system="$fs"
+  waitforcmd 2 ceph fs get "$fs"
+
   eval "$xtrace"
 }
 
@@ -99,7 +123,7 @@ helm upgrade --install \
   --version "v${VERSION}" \
   -f ./rook-ceph-cluster-values.yaml
 
-waitfor rook-ceph secret rook-ceph-dashboard-password
+waitforkube rook-ceph secret rook-ceph-dashboard-password
 set +x
 echo "===================="
 echo "dashboard passphrase"
@@ -107,13 +131,23 @@ echo "===================="
 kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo
 echo "===================="
 set -x
-#as for version 16.2.8 rook and nfs module is optional to load manually
-#https://rook.io/docs/rook/v1.9/CRDs/ceph-nfs-crd/?h=nfs#enable-the-ceph-orchestrator-if-necessary
+
+# disable ceph rook orechestrator for >= 17.2.1 and >= 16.2.11
+# https://rook.io/docs/rook/latest/CRDs/ceph-nfs-crd/#ceph-v1721
+waitforpod rook-ceph -l app=rook-ceph-tools
+ceph mgr module enable nfs
+ceph orch set backend ""
+ceph mgr module disable rook
 
 # --- customize below this line ---
 
 # lfa/s3
 kubectl apply -f s3/object_store.yaml
 kubectl apply -f s3/ingress.yaml
+
+ceph mgr module enable rook
+ceph orch set backend rook
+ceph device monitoring on
+ceph config set global device_failure_prediction_mode local
 
 # vim: tabstop=2 shiftwidth=2 expandtab
