849295e578c672146c4d587106ea4eb9815067cc
Francis Begyn <fbegyn@inuits.eu>
2023-08-29 12:27:46 +0200

    (ayekan) Configure Prometheus for Mimir remote write and deploy Mimir
    This deploys the Mimir cluster to Ayekan and ensures that the local
    prometheus writes to it. It also configures the prometheus to only use
    serviceMonitors from `o11y.eu/monitor=enabled` namespaces.

---
+++ ayekan/mimir/deploy-mimir.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+MIMIR_S3_CONFIG="mimir.structuredConfig.common.storage.s3"
+
+set -ex
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm upgrade --install ayekan-mimir grafana/mimir-distributed \
+     --set "${MIMIR_S3_CONFIG}.access_key_id=${O11Y_S3_KEY_ID}" \
+     --set "${MIMIR_S3_CONFIG}.secret_access_key=${O11Y_S3_ACCESS_KEY}" \
+     --set "${MIMIR_S3_CONFIG}.endpoint=${O11Y_S3_ENDPOINT}" \
+     --create-namespace --namespace mimir \
+     --atomic --timeout 30m0s \
+     -f ./values.yaml
