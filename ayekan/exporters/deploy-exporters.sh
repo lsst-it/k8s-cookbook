1ed8f4df45ef55d9d17ff1aa1eb2132c15d15630
Francis Begyn <fbegyn@inuits.eu>
2023-11-15 17:03:22 +0100

    (ayekan) template the namespace

--- ayekan/exporters/deploy-exporters.sh
+++ ayekan/exporters/deploy-exporters.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+NAMESPACE="monitoring"
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
@@ -13,19 +15,19 @@ if [ -z ${LSST_SNMP_COMMUNITY+x} ]; then
     echo "Please set the LSST_SNMP_COMMUNITY variable before deploying exporters"
 	exit -1
 else
-    envsubst -o ./snmp-configmap.yaml ./snmp-configmap.tmpl
+    envsubst -i ./snmp-configmap.tmpl -o ./snmp-configmap.yaml
 fi
 
-kubectl create ns monitoring --dry-run=client -oyaml | kubectl apply -f -
-kubectl --namespace monitoring apply -f snmp-configmap.yaml
+kubectl create ns ${NAMESPACE} --dry-run=client -oyaml | kubectl apply -f -
+kubectl --namespace ${NAMESPACE} apply -f snmp-configmap.yaml
 helm upgrade --install snmp-exporter prometheus-community/prometheus-snmp-exporter \
-     --create-namespace --namespace monitoring \
+     --create-namespace --namespace ${NAMESPACE} \
      --atomic --timeout 15m0s \
      --version "1.8.1" \
      -f ./snmp-exporter.yaml
 
 helm upgrade --install blackbox-exporter prometheus-community/prometheus-blackbox-exporter \
-     --create-namespace --namespace monitoring \
+     --create-namespace --namespace ${NAMESPACE} \
      --atomic --timeout 15m0s \
      --version "8.4.0" \
      -f ./blackbox-exporter.yaml
