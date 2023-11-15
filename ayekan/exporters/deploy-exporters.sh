c333d2dd1a5d7343e51dcfbaa7ea02c8ec03a4d2
Francis Begyn <fbegyn@inuits.eu>
2023-11-15 16:34:43 +0100

    (ayekan) set the SNMP community string for the SNMP exporter
    Render the SNMP string from the env variable to be set y the operator.
    Also ignore the sensitive file that might contain secrets!

--- ayekan/exporters/deploy-exporters.sh
+++ ayekan/exporters/deploy-exporters.sh
@@ -1,9 +1,20 @@
 #! /usr/bin/env bash
 
-set -ex
+set -euxo pipefail
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
+
+if [ -f ./snmp-configmap.yaml ]; then
+    rm ./snmp-configmap.yaml
+fi
+
+if [ -z ${LSST_SNMP_COMMUNITY+x} ]; then
+    echo "Please set the LSST_SNMP_COMMUNITY variable before deploying exporters"
+	exit -1
+else
+    envsubst -o ./snmp-configmap.yaml ./snmp-configmap.tmpl
+fi
 
 kubectl create ns monitoring --dry-run=client -oyaml | kubectl apply -f -
 kubectl --namespace monitoring apply -f snmp-configmap.yaml
