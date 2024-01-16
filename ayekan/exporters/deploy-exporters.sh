87730fed56469a798d369fd1b0aa0f5cc837ea1e
Francis Begyn <fbegyn@inuits.eu>
2024-01-16 21:39:46 +0100

    (ayekan) add kminion exporter for kafka

--- ayekan/exporters/deploy-exporters.sh
+++ ayekan/exporters/deploy-exporters.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 NAMESPACE="monitoring"
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add kminion https://raw.githubusercontent.com/cloudhut/kminion/master/charts/archives
 helm repo update
 
 if [ -f ./snmp-configmap.yaml ]; then
@@ -23,14 +24,18 @@ kubectl create ns ${NAMESPACE} --dry-run=client -oyaml | kubectl apply -f -
 # SNMP configuration
 kubectl --namespace ${NAMESPACE} apply -f snmp-configmap.yaml
 helm upgrade --install snmp-exporter prometheus-community/prometheus-snmp-exporter \
-     --create-namespace --namespace ${NAMESPACE} \
-     --atomic --timeout 15m0s \
+     --create-namespace --namespace ${NAMESPACE=} \
+     --atomic --timeout 15m \
      --version "1.8.1" \
      -f ./snmp-exporter.yaml
 
 # Blackbox configuration
 helm upgrade --install blackbox-exporter prometheus-community/prometheus-blackbox-exporter \
-     --create-namespace --namespace ${NAMESPACE} \
-     --atomic --timeout 15m0s \
+     --create-namespace --namespace ${NAMESPACE=} \
+     --atomic --timeout 15m \
      --version "8.4.0" \
      -f ./blackbox-exporter.yaml
+
+# kminion - kafka exporter
+kubectl --namespace ${NAMESPACE=} apply -f kminion-deployment.yaml
+kubectl --namespace ${NAMESPACE=} apply -f kminion-configmap-secret.yaml
