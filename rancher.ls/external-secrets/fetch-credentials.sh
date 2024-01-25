8ad03946880e22f368f545092f1659e5e550ff47
Joshua Hoblitt <josh@hoblitt.com>
2024-01-25 14:39:19 -0700

    (rancher.ls) add external-secrets

---
+++ rancher.ls/external-secrets/fetch-credentials.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+eval "$(op signin)"
+ONEPASS_TOKEN="$(op item get "connect.ls.lsst.org Access Token: rancher.ls.lsst.org" --fields credential)"
+
+cat > secret-onepassword-token.yaml << END
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: onepassword-connect-token
+  namespace: external-secrets
+type: Opaque
+stringData:
+  token: ${ONEPASS_TOKEN}
+END
