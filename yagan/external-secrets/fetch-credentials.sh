36cb0708803f732aa2056dc84de584bbd426aa32
Joshua Hoblitt <josh@hoblitt.com>
2024-01-26 12:51:40 -0700

    (yagan) add external-secrets

---
+++ yagan/external-secrets/fetch-credentials.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+eval "$(op signin)"
+ONEPASS_TOKEN="$(op item get "connect.cp.lsst.org Access Token: yagan.cp.lsst.org" --fields credential)"
+
+cat > secret-onepassword-token.yaml << END
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: onepassword-connect-token
+  namespace: external-secrets
+type: Opaque
+stringData:
+  token: ${ONEPASS_TOKEN}
+END
