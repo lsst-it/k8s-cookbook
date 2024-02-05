a21424f7afeeee8d01ca523194c54dea6ec7a1d7
Joshua Hoblitt <josh@hoblitt.com>
2024-02-05 12:10:30 -0700

    (kueyen) mv external-secrets to fleet

--- template/external-secrets/external-secrets.sh
+++ template/external-secrets/external-secrets.sh
@@ -2,14 +2,4 @@
 
 set -ex
 
-helm repo add external-secrets https://charts.external-secrets.io
-helm repo update
-
-helm upgrade --install \
-  external-secrets external-secrets/external-secrets \
-  --create-namespace --namespace external-secrets \
-  --version v0.9.11 \
-  --atomic
-
 kubectl apply -f secret-onepassword-token.yaml
-kubectl apply -f clustersecretstore-onepassword.yaml
