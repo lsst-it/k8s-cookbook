6839c20b1b1a44ed5663b9a6b874ec4c1340c664
Joshua Hoblitt <josh@hoblitt.com>
2024-02-13 12:05:41 -0700

    (rancher.*) bump rancher to 2.8.2

--- rancher.tu/rancher/rancher.sh
+++ rancher.tu/rancher/rancher.sh
@@ -9,7 +9,7 @@ helm upgrade --install \
   --atomic \
   rancher rancher-stable/rancher \
   --create-namespace --namespace cattle-system \
-  --version v2.7.9 \
+  --version v2.8.2 \
   -f ./values.yaml
 
 kubectl -n cattle-system rollout status deploy/rancher
