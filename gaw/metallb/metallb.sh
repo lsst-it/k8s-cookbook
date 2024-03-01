5d0cf6ebedd90e9eba7edd26c485b2e98967c2cd
Joshua Hoblitt <josh@hoblitt.com>
2024-03-01 08:23:21 -0700

    (gaw) mv metallb to fleet

--- gaw/metallb/metallb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-helm repo add metallb https://metallb.github.io/metallb
-helm upgrade --install --namespace metallb-system \
-     --create-namespace \
-    metallb metallb/metallb \
-    --atomic --version 0.13.9
-kubectl apply -f ippool.yaml
