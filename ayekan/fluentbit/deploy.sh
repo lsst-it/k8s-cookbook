22d48b99f35bee99dd0bdf864606bf2aece3fe9f
Francis Begyn <fbegyn@inuits.eu>
2024-03-13 01:16:50 +0100

    (ayekan) restructure fluentbit and opensearch directories
    Switch towards the operator frameworks in both deployments since there will
    probably be the most compatible with all of the fleet work. It's easier to manage
    the CRDs than try to work around all of the helm black holes of hope.

--- ayekan/fluentbit/deploy.sh
+++ ayekan/fluentbit/deploy.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-NAMESPACE="monitoring"
-VERSION="0.42.0"
+NAMESPACE="fluent"
+VERSION="2.7.0"
 
 set -ex
 
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
 
-helm upgrade --install ayekan-fluentbit fluent/fluent-bit \
+kubectl apply -f ./namespace.yaml
+
+helm upgrade --install fluent-operator fluent/fluent-operator \
      --create-namespace --namespace "${NAMESPACE=}" \
      --atomic --timeout 10m0s \
      --version "${VERSION=}" \
      -f ./values.yaml
 
-# kubectl --namespace "${NAMESPACE=}" apply -f ./daemonset-config.yaml
+kubectl --namespace "${NAMESPACE=}" apply -f ./configs
