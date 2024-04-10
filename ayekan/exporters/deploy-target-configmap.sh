e381cc3a2a20b9234ab816c07328824bdade002f
Joshua Hoblitt <josh@hoblitt.com>
2024-04-10 14:19:54 -0700

    (ayekan) rm manual snmp-exporter deploy; migrated to fleet

--- ayekan/exporters/deploy-target-configmap.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euxo pipefail
-
-NAMESPACE="kube-prometheus-stack"
-
-kubectl create configmap prometheus-snmp-csv-network --from-file=snmp-csv-network.json=./target-csv.json --namespace ${NAMESPACE=} -o yaml --dry-run=client | kubectl apply -f -
