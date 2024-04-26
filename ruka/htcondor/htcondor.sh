7d62d85924d40a2ce843f255bc82c58cdf2bbd3a
Carlos Barria <cbarria@yahoo.com>
2024-04-26 10:46:50 -0400

    (fleet/htcondor) add htcondor to fleet

--- ruka/htcondor/htcondor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace htcondor || true
-
-kubectl apply -f external-secret-htcondor-pool-password.yaml
-kubectl apply -f htcondor-central-manager-deployment.yaml
-kubectl apply -f htcondor-central-manager-service.yaml
-kubectl apply -f htcondor-schedd-deployment.yaml
-kubectl apply -f htcondor-worker-deployment.yaml
