141e5ef21ece85b07566dc0bb1de8b3321604ecb
Joshua Hoblitt <josh@hoblitt.com>
2024-09-25 16:49:48 -0700

    (rancher.cp) bump rancher to 2.9.1

--- rancher.cp/rancher/rancher.sh
+++ rancher.cp/rancher/rancher.sh
@@ -2,15 +2,13 @@
 
 set -ex
 
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm repo update
 
 helm upgrade --install \
-  --atomic \
   rancher rancher-latest/rancher \
   --create-namespace --namespace cattle-system \
-  --version v2.8.5 \
+  --version v2.9.1 \
   -f ./values.yaml
 
 kubectl -n cattle-system rollout status deploy/rancher
