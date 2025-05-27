5b8d4adada327b90631bff912dae298f23d522e3
Carlos Barria <cbarria@yahoo.com>
2025-05-27 16:56:55 -0400

    (rke2/rancher.tu) update/standardize rebuild config

--- rke2/rancher.tu/onepassword/onepassword-connect.sh
+++ rke2/rancher.tu/onepassword/onepassword-connect.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-set -ex
-
-kubectl create namespace onepassword-connect --dry-run=client -o yaml | kubectl apply --server-side -f -
-kubectl apply --server-side -f secret-op-credentials.yaml
+../../../template/onepassword/onepassword-connect.sh
