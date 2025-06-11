339b1473eeb87888d224b0812346456cbfea0081
Carlos Barria <cbarria@yahoo.com>
2025-06-11 10:55:23 -0400

    (template/metallb) fix script and ipaddresspool on rancher.ls

--- template/metallb/metallb.sh
+++ template/metallb/metallb.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -xe
 
+# change back to repo add because when adding --repo it does a temp and quick fetch,
+# but if the index.yaml is too big, it fails and doesnt found the version. (bug)
+
+helm repo add metallb https://metallb.github.io/metallb
+helm repo update
 helm upgrade --install metallb metallb/metallb \
-  --repo https://metallb.github.io/metallb \
   --version 0.14.9 \
-  --namespace metallb-system --create-namespace \
+  --namespace metallb-system \
+  --create-namespace \
   --atomic \
-  --timeout 600s --wait
+  --timeout 600s \
+  --wait
 
 kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=metallb -n metallb-system --timeout=180s
 
